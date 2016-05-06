378b5a127740bf25dde551cf72d2e7e2dcb97dd3
sir-kellsworth <kckswimmer@msn.com>
2016-05-05 20:38:02 -0500

    line following

--- lineFollowingTest/lineFollowingTest.ino
+++ lineFollowingTest/lineFollowingTest.ino
@@ -2,25 +2,22 @@
 #include "sensorbar.h"
 
 SensorBar bar(0x3E);
-double kp=.5;
-double ki=.5;
-double kd=.5;
+double kp=1;
+double ki=0;
+double kd=0;
 
-int RBaseSpeed=80;
-int LBaseSpeed=50;
-byte RMotor=10;
+int LBaseSpeed=65;//105;
+int RBaseSpeed=45;//75;
+byte RReverse=10;
+byte LReverse=12;
+byte RMotor=9;
 byte LMotor=11;
-
-double error=0;
-double errSum=0;
-double lastError=0;
-int lastTime=0;
-int TARGET=B00011000;
-double dErr=0;
 
 void setup() {
   pinMode(RMotor,OUTPUT);
   pinMode(LMotor,OUTPUT);
+  pinMode(RReverse,OUTPUT);
+  pinMode(LReverse,OUTPUT);
   Serial.begin(9600);
   bar.setBarStrobe();
   bar.clearInvertBits();
@@ -29,9 +26,9 @@ void setup() {
   }else{
     Serial.print("not ready");
   }
-  delay(5000);
+  delay(2000);
 }
-
+byte i = 0;
 void loop() {
   uint8_t raw=bar.getRaw();
 
@@ -39,54 +36,26 @@ void loop() {
     Serial.print(bitRead(raw,i));
   }
   Serial.println();
-  delay(500);*/
-
+  delay(500);
+  analogWrite(RMotor,i);
+  analogWrite(RMotor,0);
+  delay(100);
+  i +=1;
+  Serial.println(i);*/
   if(bitRead(raw,2)||bitRead(raw,1)||bitRead(raw,0)){
     //turn right
     analogWrite(RMotor,RBaseSpeed);
     analogWrite(LMotor,LBaseSpeed+30);
+    //Serial.println("Right");
   }else if(bitRead(raw,5)||bitRead(raw,6)||bitRead(raw,7)){
+    //turn left
     analogWrite(RMotor,RBaseSpeed+30);
     analogWrite(LMotor,LBaseSpeed);
+    //Serial.println("Left");
   }else if(bitRead(raw,3)||bitRead(raw,4)){
     analogWrite(RMotor,RBaseSpeed);
     analogWrite(LMotor,LBaseSpeed);
   }
 
-    /*unsigned long now=millis();
-    unsigned long timeChange=(now-lastTime);
-
-    error=TARGET-raw;
-    errSum+=(error*timeChange);
-    dErr=(error-lastError)/timeChange;
-
-    lastError=error;  
-    lastTime=now;
-
-    //TODO figure out how to write result to motors
-    double result=kp*error+ki*errSum+kd*dErr;
-
-    int RMotorSpeed=RBaseSpeed+result;
-    int LMotorSpeed=LBaseSpeed-result;
-
-    if(RMotorSpeed>130){
-      RMotorSpeed=130;
-    }
-
-    if(RMotorSpeed<50){
-      RMotorSpeed=50;
-    }
-
-    if(LMotorSpeed>100){
-      LMotorSpeed=100;
-    }
-
-    if(LMotorSpeed<25){
-      LMotorSpeed=25;
-    }
-    
-    analogWrite(RMotor,RMotorSpeed);
-    analogWrite(LMotor,LMotorSpeed);*/
-
-    delayMicroseconds(100);
+    delayMicroseconds(10000);
 }
